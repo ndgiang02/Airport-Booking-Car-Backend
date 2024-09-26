71e03ec310855e52ea74ea937dc3a2af113b5c07
giang <muaha.vang02@gmail.com>
2024-09-27 00:13:14 +0700

    update

---
+++ app/Models/Test.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DriverResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'license_no' => $this->license_no,
+            'rating' => $this->rating,
+            'available' => $this->available,
+            'vehicle' => new VehicleResource($this->whenLoaded('vehicle')), 
+        ];
+    }
+}
+
